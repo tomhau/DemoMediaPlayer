ea373989c5e2d24b3c6e7e4eabdd6315b1e8972f
Tommy Haugaard <tha@easv.dk>
2018-12-14 11:19:36 +0100

    Initial Commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,54 @@
+package sample;
+
+import javafx.scene.media.*;
+import javafx.scene.control.Button;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import java.io.*;
+import java.net.*;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private MediaView mediaV;
+
+    @FXML
+    private Button play;
+
+    private MediaPlayer mp;
+    private Media me;
+
+
+    /**
+     * This method is invoked automatically in the beginning. Used for initializing, loading data etc.
+     *
+     * @param location
+     * @param resources
+     */
+    public void initialize(URL location, ResourceBundle resources){
+        // Build the path to the location of the media file
+        String path = new File("src/sample/media/SampleAudio_0.4mb.mp3").getAbsolutePath();
+        // Create new Media object (the actual media content)
+        me = new Media(new File(path).toURI().toString());
+        // Create new MediaPlayer and attach the media to be played
+        mp = new MediaPlayer(me);
+        //
+        mediaV.setMediaPlayer(mp);
+        // mp.setAutoPlay(true);
+        // If autoplay is turned of the method play(), stop(), pause() etc controls how/when medias are played
+        mp.setAutoPlay(false);
+
+    }
+
+    @FXML
+    /**
+     * Handler for the play button
+     */
+    private void handlePlay()
+    {
+        // Play the mediaPlayer with the attached media
+        mp.play();
+    }
+
+}
