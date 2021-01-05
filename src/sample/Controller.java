c64c0694d5d21f8a7255e5cdf1140a96227040b9
Swaiidan <71323144+Swaiidan@users.noreply.github.com>
2021-01-06 00:17:06 +0100

    Add files via upload

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,54 +1,34 @@
 package sample;
-//test
-import javafx.scene.media.*;
-import javafx.scene.control.Button;
 
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import java.io.*;
-import java.net.*;
-import java.util.ResourceBundle;
+public class Controller {
 
-public class Controller implements Initializable {
-    @FXML
-    private MediaView mediaV;
-
-    @FXML
-    private Button play;
-
-    private MediaPlayer mp;
-    private Media me;
-
-
-    /**
-     * This method is invoked automatically in the beginning. Used for initializing, loading data etc.
-     *
-     * @param location
-     * @param resources
-     */
-    public void initialize(URL location, ResourceBundle resources){
-        // Build the path to the location of the media file
-        String path = new File("src/sample/media/file_example_MP4_640_3MG.mp4").getAbsolutePath();
-        // Create new Media object (the actual media content)
-        me = new Media(new File(path).toURI().toString());
-        // Create new MediaPlayer and attach the media to be played
-        mp = new MediaPlayer(me);
-        //
-        mediaV.setMediaPlayer(mp);
-        // mp.setAutoPlay(true);
-        // If autoplay is turned of the method play(), stop(), pause() etc controls how/when medias are played
-        mp.setAutoPlay(false);
-
+    public void HandlePlay() {
+        System.out.println("It works.... ");
     }
 
-    @FXML
-    /**
-     * Handler for the play button
-     */
-    private void handlePlay()
-    {
-        // Play the mediaPlayer with the attached media
-        mp.play();
+    public void HandleStop() {
+        System.out.println("It works.... ");
     }
 
-}
+    public void HandlePause() {
+        System.out.println("It works.... ");
+    }
+    public void HandleNext() {
+        System.out.println("It works.... ");
+    }
+    public void HandlePrevious() {
+        System.out.println("It works.... ");
+    }
+    public void HandleReplay() {
+        System.out.println("It works.... ");
+    }
+    public void HandleSearch() {
+        System.out.println("It works.... ");
+    }
+    public void HandleAddPlaylist() {
+        System.out.println("It works.... ");
+    }
+    }
+
+
+
