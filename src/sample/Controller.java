8c42afac7194ead4589940c0a51f25b18f257e5e
AndSimi <71438281+AndSimi@users.noreply.github.com>
2021-01-06 22:38:44 +0100

    Add files via upload
    Added select file button to GUI and made it functional
    Made start/stop/pause  buttons functional

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,17 +1,31 @@
 package sample;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.FileChooser;
+import java.io.File;
+import javax.swing.*;
+import java.util.ResourceBundle;
 
 public class Controller {
+    private String path;
+    private MediaPlayer mediaPlayer;
 
-    public void HandlePlay() {
-        System.out.println("It works.... ");
+
+
+    public void HandlePlay(ActionEvent event) {
+        mediaPlayer.play();
+
     }
 
     public void HandleStop() {
-        System.out.println("It works.... ");
+        mediaPlayer.stop();
     }
 
-    public void HandlePause() {
-        System.out.println("It works.... ");
+    public void HandlePause(ActionEvent event) {
+        mediaPlayer.pause();
     }
     public void HandleNext() {
         System.out.println("It works.... ");
@@ -28,6 +42,36 @@ public class Controller {
     public void HandleAddPlaylist() {
         System.out.println("It works.... ");
     }
+
+    /**
+     * Creating a fileChooser in order to select a desired file from your computer
+     * We use path in order to get the path where the file is located
+     *
+     */
+
+    public void HandleChooseFile(ActionEvent event){
+
+    FileChooser fileChooser = new FileChooser();
+    File fileToOpen = fileChooser.showOpenDialog(null);
+    path = fileToOpen.toURI().toString();
+
+    //Checking if path contains something and is not null
+        if(path !=null){
+            //The selected file(path) is used as an argument for media
+            //The media is then used with Media Player
+             Media media = new Media(path);
+             mediaPlayer = new MediaPlayer(media);
+             //mediaView.setMediaPlayer(mediaPlayer);
+            // mediaPlayer.play();
+
+        }
+
+
+
+    }
+
+
+
     }
 
 
