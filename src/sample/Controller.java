b71efbbdb685f5665230b50cb528fb2183c093be
Swaiidan <71323144+Swaiidan@users.noreply.github.com>
2021-01-11 03:05:00 +0100

    Add files via upload

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,19 +1,67 @@
 package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
 import java.io.File;
-import javax.swing.*;
-import java.util.ResourceBundle;
+import java.io.IOException;
+
+
+//This item is for the ListView all the song
+
 
 public class Controller {
+
+
     private String path;
     private MediaPlayer mediaPlayer;
 
 
+
+    public void ListViewAllSongs(Stage stage) throws Exception {
+
+        //Select Data from the Database
+        DB.selectSQL("Select * from tbl_Songs");
+
+        //Get the data
+
+        do {
+            String SongNameData = DB.getDisplayData();
+            if (SongNameData.equals(DB.NOMOREDATA)) {
+                break;
+            } else {
+                System.out.print(SongNameData);
+            }
+        } while (true);
+
+
+        //Create the ListView Control:
+
+        //1. Create the List for the Listview
+        ObservableList<String> NamesList = FXCollections.observableArrayList("Test Test Test");
+
+        //2. Create the ListView
+        ListView TestList= new ListView();
+
+
+        //3. Creating a scene
+
+
+}
+
+public void ListViewPlayList(Stage primaryStage) throws Exception {
+
+}
 
     public void HandlePlay(ActionEvent event) {
         mediaPlayer.play();
@@ -27,18 +75,23 @@ public class Controller {
     public void HandlePause(ActionEvent event) {
         mediaPlayer.pause();
     }
+
     public void HandleNext() {
-        System.out.println("It works.... ");
+
     }
+
     public void HandlePrevious() {
         System.out.println("It works.... ");
     }
+
     public void HandleReplay() {
         System.out.println("It works.... ");
     }
+
     public void HandleSearch() {
         System.out.println("It works.... ");
     }
+
     public void HandleAddPlaylist() {
         System.out.println("It works.... ");
     }
@@ -46,32 +99,29 @@ public class Controller {
     /**
      * Creating a fileChooser in order to select a desired file from your computer
      * We use path in order to get the path where the file is located
-     *
      */
 
-    public void HandleChooseFile(ActionEvent event){
+    public void HandleChooseFile(ActionEvent event) {
 
-    FileChooser fileChooser = new FileChooser();
-    File fileToOpen = fileChooser.showOpenDialog(null);
-    path = fileToOpen.toURI().toString();
+        FileChooser fileChooser = new FileChooser();
+        File fileToOpen = fileChooser.showOpenDialog(null);
+        path = fileToOpen.toURI().toString();
 
-    //Checking if path contains something and is not null
-        if(path !=null){
+        //Checking if path contains something and is not null
+        if (path != null) {
             //The selected file(path) is used as an argument for media
             //The media is then used with Media Player
-             Media media = new Media(path);
-             mediaPlayer = new MediaPlayer(media);
-             
+            Media media = new Media(path);
+            mediaPlayer = new MediaPlayer(media);
+
 
         }
 
 
-
     }
 
+}
 
-
-    }
 
 
 
